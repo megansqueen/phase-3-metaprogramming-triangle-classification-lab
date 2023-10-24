19f84003acb50209f8d3b85fa018029f968687ff
Megan <esler6724@gmail.com>
2023-10-23 18:19:33 -0700

    commit tree 97f23c0f64c6fbb0e1a30a1374a723bd4399e42f

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,27 @@
 class Triangle
-  # write code here
+  attr_accessor :equilateral
+  attr_accessor :isosceles
+  attr_accessor :scalene
+
+  @@a = 0
+  @@b= 0
+  @@c = 0
+  
+  def initialize(side_1, side_2, side_3)
+    @@a = side_1
+    @@b= side_2
+    @@c = side_3
+    @@sides = [@@a, @@b, @@c]
+  end
+
+  def kind
+    if @@sides.uniq.size == 1
+      :equilateral
+    elsif @@sides.uniq.size == 2
+      :isosceles
+    elsif @@sides.uniq.size == 3
+      :scalene
+    end
+  end
+
 end
